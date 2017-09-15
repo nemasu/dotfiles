7d2a595ee8f8504590e240201100a872a7e85e64
nemasu <nemasu@gmail.com>
2017-09-15 15:15:25 +0900

    vim-devicons, Terminess font.

--- setup.sh
+++ setup.sh
@@ -1,21 +1,39 @@
 #!/bin/bash
 
+current_dir=`pwd`
+
+if [ -e ~/.Xresources ]; then
+	echo "~/.Xresources already exists, skipping..."
+else
+    ln -vs $current_dir/X/.Xresources ~/.Xresources
+fi
+
+
 if [ -e ~/.screenrc ]; then
-	echo "~/.screenrc already exists, exiting."
-	exit 1
+	echo "~/.screenrc already exists, skipping..."
+else
+    ln -vs $current_dir/screen/screenrc ~/.screenrc 
 fi
 
 if [ -e ~/.vim ]; then
-	echo "~/.vim already exists, exiting."
-	exit 1
+	echo "~/.vim already exists, skipping..."
+else
+    ln -vs $current_dir/vim      ~/.vim 
 fi
 
 if [ -e ~/.vimrc ]; then
-	echo "~/.vimrc already exists, exiting."
-	exit 1
+	echo "~/.vimrc already exists, skipping..."
+else
+    ln -vs $current_dir/vim/vimrc    ~/.vimrc
 fi
 
-current_dir=`pwd`
-ln -s $current_dir/screen/screenrc ~/.screenrc 
-ln -s $current_dir/vim      ~/.vim 
-ln -s $current_dir/vim/vimrc    ~/.vimrc 
+if [ ! -e ~/.local/share/fonts ]; then
+    mkdir -p ~/.local/share/fonts
+fi
+
+if [ -e ~/.local/share/fonts/Terminess\ Nerd\ Font\ Complete\ Mono.ttf ]; then
+    echo "Terminess font exists already, skipping..."
+else
+    ln -vs $current_dir/fonts/Terminess\ Nerd\ Font\ Complete\ Mono.ttf ~/.local/share/fonts
+fi
+
